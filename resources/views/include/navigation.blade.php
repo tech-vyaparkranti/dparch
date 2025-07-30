a66f7c8b9d8ff4cb9a92ddfa1366ce58f590fc26
prabhat <prabhatsinghgkp123@gmail.com>
2025-07-30 11:43:52 +0530

    changes in navigation section

--- resources/views/include/navigation.blade.php
+++ resources/views/include/navigation.blade.php
@@ -33,10 +33,10 @@
       About Us
     </a>
     <ul class="dropdown">
-      <li><a href="{{ route('aboutUs') }}#about" onclick="refreshToSection('about')">Introduction</a></li>
-      <li><a href="{{ route('aboutUs') }}#team" onclick="refreshToSection('team')">The Team</a></li>
-      <li><a href="{{ route('aboutUs') }}#philosophy" onclick="refreshToSection('philosophy')">Our Foundation</a></li>
-      <li><a href="{{ route('aboutUs') }}#services" onclick="refreshToSection('services')">What We Do</a></li>
+      <li><a href="{{ route('aboutUs') }}#about">Introduction</a></li>
+      <li><a href="{{ route('aboutUs') }}#team" >The Team</a></li>
+      <li><a href="{{ route('aboutUs') }}#philosophy" >Our Foundation</a></li>
+      <li><a href="{{ route('aboutUs') }}#services" >What We Do</a></li>
     </ul>
   </li>
 
